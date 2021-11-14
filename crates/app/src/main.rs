a65e250c401668ccc4eccb1f95337080c6ac5a2e
Jonas Buergel <burgeljonas@gmail.com>
2021-11-14 23:41:49 +0100

    setup templates/database

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{routing::get, AddExtensionLayer, Router, Server};
+use sea_orm::Database;
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+const ADDRESS: &str = "127.0.0.1:8080";
+const DATABASE_URL: &str = "sqlite://db.sqlite";
+
+#[tokio::main]
+async fn main() {
+    let address = SocketAddr::from_str(ADDRESS).expect("invalid ip:port");
+    let database = Database::connect(DATABASE_URL)
+        .await
+        .expect("Database connection failed");
+
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .layer(AddExtensionLayer::new(database));
+
+    Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .expect("could not start server...");
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+async fn hello_world() -> String {
+    let template = HelloTemplate { name: "name" };
+    template.render().unwrap()
 }
