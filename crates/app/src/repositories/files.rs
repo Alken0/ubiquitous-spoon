f496218ce5abfc75f8ddc2152d5018fb71cf2f2e
Jonas Buergel <burgeljonas@gmail.com>
2022-01-13 22:18:39 +0100

    FIX sqlite-limitation too many inserts at once

--- crates/app/src/repositories/files.rs
+++ crates/app/src/repositories/files.rs
@@ -33,18 +33,23 @@ impl Files {
     }
 
     pub async fn insert_all(&self, files: Vec<InsertFile>) -> Result<(), String> {
-        // Entity::insert_many returns sql-error if vector is empty
-        if files.is_empty() {
-            return Ok(());
+        // SQLITE-Limitation: throws error "Execution Error: error returned from database: too many SQL variables" if there are too many inserts at once
+        let mut inserts = files.into_iter().map(|e| e.active_model).peekable();
+        while inserts.peek().is_some() {
+            let chunk: Vec<file::ActiveModel> = inserts.by_ref().take(1024).collect();
+            let result = file::Entity::insert_many(chunk)
+                .exec(&self.db)
+                .await
+                .map(|_| ())
+                .map_err(|e| e.to_string());
+
+            if result.is_err() {
+                println!("{}", result.as_ref().err().unwrap());
+                return result;
+            }
         }
 
-        let inserts: Vec<file::ActiveModel> = files.into_iter().map(|e| e.active_model).collect();
-
-        return file::Entity::insert_many(inserts)
-            .exec(&self.db)
-            .await
-            .map(|_| ())
-            .map_err(|e| e.to_string());
+        Ok(())
     }
 
     pub async fn delete_by_path(&self, path: &str) -> Result<(), String> {
